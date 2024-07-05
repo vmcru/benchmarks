4fc5e387ad4925abd11f2a9949ea8957448a19e6
vmcru <victor.cruz.hrdz@gmail.com>
2024-07-05 01:14:30 -0400

    added models and other fixes

--- benchmarks/MOABB/models/BrainGraph.py
+++ benchmarks/MOABB/models/BrainGraph.py
@@ -3,6 +3,7 @@ from torch_geometric.nn import GCNConv, ChebConv, GraphConv, global_mean_pool
 import torch.nn.functional as F
 from torch.nn import Linear
 import torch_geometric.nn.models as models
+from torch_geometric.transforms.add_positional_encoding import AddLaplacianEigenvectorPE, AddRandomWalkPE
 
 
 
@@ -23,7 +24,20 @@ def from_static(x, edges):
     return batch
 
 class GCN(torch.nn.Module):
-    def __init__(self, kdeg1, kdeg2, kdeg3, hidden_channels1, hidden_channels2, hidden_channels3, num_node_features, num_classes, channels, activation_type="elu", test=0):
+    def __init__(self, 
+                num_classes, 
+                channels, 
+                kdeg1=1, 
+                kdeg2=1, 
+                kdeg3=1, 
+                hidden_channels1=16, 
+                hidden_channels2=16, 
+                hidden_channels3=16, 
+                num_node_features=16, 
+                activation_type="elu", 
+                test=0, 
+                num_layers=1, 
+                dropout=0):
         super(GCN, self).__init__()
 
         if activation_type == "gelu":
@@ -43,13 +57,41 @@ class GCN(torch.nn.Module):
 
         if test==1:
             self.test = test
-            self.model = models.GCN(in_channels=num_node_features, hidden_channels=hidden_channels1, out_channels=hidden_channels3,num_layers=4)
+            self.model = models.GCN(in_channels=num_node_features, 
+                                    hidden_channels=hidden_channels1, 
+                                    out_channels=hidden_channels3,
+                                    num_layers=num_layers,
+                                    dropout=dropout,
+                                    act=activation_type)
         elif test==2:
             self.test = test
-            self.model = models.GraphSAGE(in_channels=num_node_features, hidden_channels=hidden_channels1, out_channels=hidden_channels3,num_layers=2)
+            self.model = models.GraphSAGE(in_channels=num_node_features, 
+                                        hidden_channels=hidden_channels1, 
+                                        out_channels=hidden_channels3,
+                                        num_layers=num_layers, 
+                                        dropout=dropout,
+                                        act=activation_type)
         elif test==3:
             self.text = test
-            self.model = models.GAT(in_channels=num_node_features, hidden_channels=hidden_channels1, out_channels=hidden_channels3,num_layers=2)
+            self.model = models.GAT(in_channels=num_node_features, 
+                                    hidden_channels=hidden_channels1, 
+                                    out_channels=hidden_channels3,
+                                    num_layers=num_layers, 
+                                    dropout=dropout,
+                                    act=activation_type,
+                                    heads = 2)
+        elif test==4:
+            self.text = test
+            self.model = models.PNA(in_channels=num_node_features, 
+                                    hidden_channels=hidden_channels1, 
+                                    out_channels=hidden_channels3,
+                                    num_layers=num_layers, 
+                                    dropout=dropout,
+                                    act=activation_type,
+        #                            aggregators = , 
+        #                            scalers = 
+        #                            deg = 2
+                                    )
         else:    
             self.conv1 = ChebConv(num_node_features, hidden_channels1,kdeg1)
             self.conv2 = ChebConv(hidden_channels1, hidden_channels2,kdeg2)
@@ -65,6 +107,7 @@ class GCN(torch.nn.Module):
         edge_index = edge_index[0]
         if self.test!=0 :
             batch_new = from_static(x, edge_index)
+            #AddLaplacianEigenvectorPE()
             x = self.model(batch_new.x, batch_new.edge_index.to(x.device))
 
         else:
