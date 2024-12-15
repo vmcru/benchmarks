0b13b6ef50e9a748e5650927e80152fda3be2022
Victor Cruz <vcruz@narval3.narval.calcul.quebec>
2024-12-14 20:15:10 -0500

    general commit for the files :w :wx :q
    a Please enter the commit message for your changes. Lines starting

--- benchmarks/MOABB/train.py
+++ benchmarks/MOABB/train.py
@@ -90,14 +90,14 @@ class MOABBBrain(sb.Brain):
             + tuple(np.floor(self.hparams.input_shape[1:-1]).astype(int))
             + (1,)
         )
-        if not self.hparams.braindecode:
-            model_summary = summary(
-                self.hparams.model, input_size=in_shape, device=self.device
-            )
-            with open(
-                os.path.join(self.hparams.exp_dir, "model.txt"), "w"
-            ) as text_file:
-                text_file.write(str(model_summary))
+
+        model_summary = summary(
+            self.hparams.model, input_size=in_shape, device=self.device
+        )
+        with open(
+            os.path.join(self.hparams.exp_dir, "model.txt"), "w"
+        ) as text_file:
+            text_file.write(str(model_summary))
 
     def on_stage_start(self, stage, epoch=None):
         "Gets called when a stage (either training, validation, test) starts."
